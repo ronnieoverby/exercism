f748af7163a38812d95b5e8cea8bb5393f3f2a14
Ronnie Overby <roverby@coretechs.net>
2016-01-07 12:11:26 -0500

    elixir submit dna

--- elixir/nucleotide-count/dna.exs
+++ elixir/nucleotide-count/dna.exs
@@ -12,9 +12,13 @@ defmodule DNA do
   iex> DNA.count('AATAA', ?T)
   1
   """
-  @spec count([char], char) :: non_neg_integer
-  def count(strand, nucleotide) do
+  @spec count([char], char) :: non_neg_integer  
+  def count(strand, nucleotide) when nucleotide in @nucleotides do 
+    strand |> histogram |> Map.get(nucleotide)
+  end
 
+  def count(_strand, nucleotide) do
+    raise ArgumentError, message: "invalid nucleotide: #{nucleotide}"
   end
 
 
@@ -27,7 +31,16 @@ defmodule DNA do
   %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
   """
   @spec histogram([char]) :: Dict.t
-  def nucleotide_counts(strand) do
+  def histogram(strand) do
+    map = Map.new(@nucleotides, fn k -> {k, 0} end)
+    Enum.reduce(strand, map, &increment_histogram/2)
+  end
 
+  defp increment_histogram(key, map) when key in @nucleotides do
+    Map.update(map, key, 0, &(&1 + 1))
+  end
+
+  defp increment_histogram(key, _map) do
+    raise ArgumentError, message: "invalid nucleotide found in dna strand: #{key}"
   end
 end

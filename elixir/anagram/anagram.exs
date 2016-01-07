ba0a89894cab70344547b3c77ea478622c877adc
Ronnie Overby <ronnie@ronnieo.net>
2016-01-07 07:27:12 -0500

    elixir: submit anagram

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -2,8 +2,23 @@ defmodule Anagram do
   @doc """
   Returns all candidates that are anagrams of, but not equal to, 'base'.
   """
+
+  import String
+
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
-  
+  	
+  	up_base = upcase(base)  	
+
+  	for candidate <- candidates,
+  		up_cand = upcase(candidate),		
+  		up_base !== up_cand,
+  		base_list = sort(up_base),
+  		cand_list = sort(up_cand),
+  		base_list === cand_list,
+  		do: candidate
   end
+
+  defp sort(s), do: s |> graphemes |> Enum.sort
+  
 end

2a31aeed5f8e25d8424c4b66df80bc313102dd5e
Ronnie Overby <ronnie@ronnieo.net>
2016-01-07 19:31:34 -0500

    elixir submit point_mutations

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -9,5 +9,12 @@ defmodule DNA do
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
   def hamming_distance(strand1, strand2) do
+  	case length(strand1) === length(strand2) do
+  	   false -> nil
+  	   true ->
+	  	strand1
+	  	|> Enum.zip(strand2)
+	  	|> Enum.count(fn {a,b} -> a !== b end)
+  	end
   end
 end

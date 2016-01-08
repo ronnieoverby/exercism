e71e1134d3e368c54d6044b1604909193dbe6c91
Ronnie Overby <ronnie@ronnieo.net>
2016-01-07 20:56:48 -0500

    elixir submit phone_numbers fetched grade school

--- elixir/phone-number/phone_number.exs
+++ elixir/phone-number/phone_number.exs
@@ -1,4 +1,7 @@
 defmodule Phone do
+
+  import String
+
   @doc """
   Remove formatting from a phone number.
 
@@ -18,8 +21,14 @@ defmodule Phone do
   """
   @spec number(String.t) :: String.t
   def number(raw) do
-
+    digits = replace(raw, ~r/[^[:alnum:]]/, "")
+    _number(digits, String.length(digits) )
   end
+
+  defp _number(digits, 10), do: digits
+  defp _number("1" <> last_ten, 11), do: last_ten
+  defp _number(_,_), do: "0000000000"
+
 
   @doc """
   Extract the area code from a phone number
@@ -40,7 +49,7 @@ defmodule Phone do
   """
   @spec area_code(String.t) :: String.t
   def area_code(raw) do
-  
+    raw |> number |> slice(0,3)
   end
 
   @doc """
@@ -62,6 +71,10 @@ defmodule Phone do
   """
   @spec pretty(String.t) :: String.t
   def pretty(raw) do
-  
+    num = number(raw)
+    a = slice(num, 0..2)
+    b = slice(num, 3..5)
+    c = slice(num, 6..9)
+    "(#{a}) #{b}-#{c}"
   end
 end

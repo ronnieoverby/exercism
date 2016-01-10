d4388d6b62cdd6794d497c0ddc33f14792531533
Ronnie Overby <ronnie@ronnieo.net>
2016-01-10 15:07:48 -0500

    elixir submit triangle

--- elixir/triangle/triangle.exs
+++ elixir/triangle/triangle.exs
@@ -4,8 +4,29 @@ defmodule Triangle do
   @doc """
   Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
   """
-  @spec kind(number, number, number) :: { :ok, kind } | { :error, String.t }
+  @spec kind(number, number, number) :: { :ok, kind } | { :error, String.t }  
   def kind(a, b, c) do
 
+  	cond do
+  	  Enum.any?([a,b,c], &(&1 <= 0)) ->
+  	  	{:error, "all side lengths must be positive"}
+
+	  not triangle_inequality(a,b,c) ->	  	
+  	  	{:error, "side lengths violate triangle inequality"}
+
+	  true ->
+	  	case [a,b,c] |> Enum.uniq |> Enum.count do
+	  	   1 -> {:ok, :equilateral}
+	  	   2 -> {:ok, :isosceles }
+	  	   3 -> {:ok, :scalene}  	    
+	  	end
+  	    
+  	end
+
+  end
+
+  defp triangle_inequality(a, b, c) do
+  	[a,b,c] = Enum.sort [a,b,c]
+  	a + b > c
   end
 end
